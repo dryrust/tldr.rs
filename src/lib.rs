2b6ca3d2ea9fd00132cdc96b11e3d01e366bc223
Arto Bendiken <arto@bendiken.net>
2025-08-04 17:45:01 +0400

    Bump the version to 0.0.1.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 // This is free and unencumbered software released into the public domain.
 
-//! This crate provides abstractions for TL;DR summarization.
+//! This crate provides abstractions for TL;DR summarization using the five Ws.
 
 #![no_std]
 #![forbid(unsafe_code)]
